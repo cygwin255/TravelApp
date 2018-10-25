abed591e104f33fb20c7f2f78df54be341665c84
Andrei Ledauski <andrei.ledauski@itechart-group.com>
2018-10-25 14:25:50 +0300

    Added delete confirmation modal. Rebuild docs

--- src/app/travel-history/travel-history.component.ts
+++ src/app/travel-history/travel-history.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
 import { TravelHistoryService } from '../shared/travel-history-service/travel-history.service';
 import { HistoryType } from '../shared/travel-history-service/travel-history.model';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 @Component({
   selector: 'app-travel-history',
   templateUrl: './travel-history.component.html',
@@ -10,6 +12,17 @@ export class TravelHistoryComponent {
   Array = Array;
   HistoryType = HistoryType;
 
-  constructor(public historyService: TravelHistoryService) {
+  constructor(public historyService: TravelHistoryService,
+              private modalService: NgbModal) {
+  }
+
+  openDeleteConfirmation(index: number, modal: TemplateRef<any>) {
+    this.modalService.open(modal)
+      .result
+      .then((result) => {
+        if (result) {
+          this.historyService.remove(index);
+        }
+      }, () => {});
   }
 }
